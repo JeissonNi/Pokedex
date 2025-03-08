22f704e5d3a0b650c3900125056907f082a9463b
JeissonNi <jeisson.e.96@hotmail.com>
2025-03-08 18:41:57 -0500

    Imagen y botón ejemplo index.html script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-async function obtenerDatos() {
-    try {
-      let response = await fetch('https://pokeapi.co/api/v2/pokemon/151');
-      let data = await response.json();
-      console.log(data["name"]);
-    } catch (error) {
-      console.error('Error:', error);
-    }
+async function buscar() {
+  try {
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/151");
+    let data = await response.json();
+    
+    let imgPokemon = document.getElementById("imgPokemon");
+    //console.log(data["sprites"].front_default);
+    imgPokemon.src = data["sprites"].front_default;
+
+  } catch (error) {
+    console.error("Error:", error);
   }
-  
-  obtenerDatos();
-  
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let botonBuscar = document.getElementById("btnBuscar");
+  botonBuscar.onclick = buscar;
+});
