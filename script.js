24cdd53b156ccc221cc0e8f0c5020e68855f821b
JeissonNi <jeisson.e.96@hotmail.com>
2025-03-08 19:08:01 -0500

    Buscador funcional index.html script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 async function buscar() {
-  try {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/151");
-    let data = await response.json();
-    
-    let imgPokemon = document.getElementById("imgPokemon");
-    //console.log(data["sprites"].front_default);
-    imgPokemon.src = data["sprites"].front_default;
+    try {
+      let imgPokemon = document.getElementById("imgPokemon");
+      let entradaPokemon = document.getElementById("entradaPokemon");
 
-  } catch (error) {
-    console.error("Error:", error);
+  
+      if (entradaPokemon.value.trim() !== "") {
+          let url = "https://pokeapi.co/api/v2/pokemon/" + entradaPokemon.value.toLowerCase(); 
+          
+          let response = await fetch(url);
+  
+          if (!response.ok) {
+            throw new Error("Pokémon no encontrado");
+          }
+  
+          let data = await response.json();
+          imgPokemon.src = data.sprites.front_default;
+      } else {
+          alert("No has ingresado un Pokémon válido");
+      }
+  
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Hubo un error en la búsqueda. Verifica el nombre e intenta de nuevo.");
+    }
   }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  let botonBuscar = document.getElementById("btnBuscar");
-  botonBuscar.onclick = buscar;
-});
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    let botonBuscar = document.getElementById("btnBuscar");
+    botonBuscar.onclick = buscar;
+  });
+  
